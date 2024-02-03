89cd6b9d0792b3171ab88a9e607322dc402fe59e
laurabacon <laura_kessinger@yahoo.com>
2024-02-03 16:23:31 -0700

    thought and reaction models

--- models/thought.js
+++ models/thought.js
@@ -1,8 +1,70 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+//need to get date
+
+//mongoose schema instance
+const reactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId(),
+        required: true,
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
 
 
+//schema instance
+  const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+  
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      username: [
+        {
+          type: String,
+          required: true,
+        },
+      ],
+      reactions: [reactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
 
-
+  thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
 
 
 
